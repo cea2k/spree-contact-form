f79fa1abb499b730d0dcee37ff6d42ed73fc6bd3
Carlos E. Alarcón <cea2k@hotmail.com>
2011-10-11 12:29:44 -0300

    Updating engine initialization and compatibility with spree 0.70

--- lib/spree_contact_form_hooks.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "admin/configurations/index",
-                     :name => "contact_topic_configuration_option",
-                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-                     :text => "<%= configurations_menu_item(I18n.t('contact_topics'), admin_contact_topics_path, I18n.t('contact_topics_description')) %>",
-                     :disabled => false)
