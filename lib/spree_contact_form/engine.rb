32e140ce4a2829dc7504e8720096e6bebc45a349
Carlos E. Alarcón <cea2k@hotmail.com>
2011-10-11 12:27:34 -0300

    Updating engine initialization and compatibility with spree 0.70

--- lib/spree_contact_form/engine.rb
+++ lib/spree_contact_form/engine.rb
@@ -1,12 +1,17 @@
 require "spree_contact_form"
+require "deface"
 
 module SpreeContactForm
 
   class Engine < Rails::Engine
 
     def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
     end
 
+    config.to_prepare &method(:activate).to_proc
   end
 
 end
