ea75dd58f9f2c95168337d3261aa31e73f7a635b
Carlos E. Alarcon <cea2k@hotmail.com>
2011-12-26 21:18:33 -0300

    agregando llave de locale para el asunto del mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
   helper "spree/base"
 
   def message_email(message)
-    subject = "#{Spree::Config[:site_name]} - Message from #{message.email}"
+    subject = "#{Spree::Config[:site_name]} - " + I18n.t(:mail_subject, :email => message.email)
 
     @message = message
     mail(:to => message.topic.emails, :subject => subject, :reply_to => message.email) 
